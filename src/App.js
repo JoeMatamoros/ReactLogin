9558223992a1641206f3e3a9f707c7e9e3c6cfe4
Jose Ordonez <joseaordoez@gmail.com>
2021-03-07 20:39:08 -0600

    Add file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 import BrandHeader from './components/BrandHeader/BrandHeader';
-import Cards from './components/Cards/Cards';
-import CardHolder from './components/Cards/CardHolder';
 import Login from './components/Login/Login';
+import Home from './components/Pages/Home';
+import Todo from './components/Pages/Todo/Todo';
+
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+//import Cards from './components/Cards/Cards';
+//import CardHolder from './components/Cards/CardHolder';
+
 
 function App() {
   return (
-    <section>
-      <BrandHeader></BrandHeader>
-      <section className="Login">
-        <Login></Login>
+    <Router>
+      <section>
+        <BrandHeader></BrandHeader>
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+
+            <Route path="/todos" exact>
+              <Todo />
+            </Route>
+            
+            <Route path="/login" exact>
+                <Login></Login>
+            </Route>
+        </Switch>
       </section>
-    </section>
+    </Router>
   );
 }
 
 export default App;
+
+/*
+<section>
+        <img src={imgMeme} alt="Un meme" />
+      </section>
+      <CardHolder>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+      </CardHolder>
+*/
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+<section>
+        <h1 class="text-center m-4">ALGO COOL HECHO EN REACT SE PONDRA</h1>
+    </section>
+
+    );    
+}
+export default Home;
